7ee42f11fc5b276d9d679b99d8ce9e80fbcdbbcd
Joshua Owusu <joshbillion1@gmail.com>
2023-06-09 09:51:52 +0000

    Commit auto_update

--- Disney/src/App.jsx
+++ Disney/src/App.jsx
@@ -1,7 +1,6 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-// import './App.css'
 
 // function App() {
 //   const [count, useCount] = useState();
@@ -16,6 +15,8 @@
 // export default App
 
 import React from 'react'
+import './App.css'
+
 import Header from './components/Header'
 
 function App() {
