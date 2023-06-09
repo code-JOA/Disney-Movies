0cb3dde2182444437a4005b63f10c9506d831495
Joshua Owusu <joshbillion1@gmail.com>
2023-06-09 09:23:47 +0000

    Commit auto_update

--- Disney/src/App.jsx
+++ Disney/src/App.jsx
@@ -19,9 +19,11 @@ import React from 'react'
 
 function App() {
   return (
-    <div className="app">
-      <
-      <Header />
+    <div className=>
+      <h1 className="text-3xl font-bold underline">
+        Hello world
+      </h1>
+      {/* <Header /> */}
     </div>
   )
 }
