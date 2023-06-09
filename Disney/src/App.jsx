071a48c7d565512e83b1ccdcd669699f8d32cd6f
Joshua Owusu <joshbillion1@gmail.com>
2023-06-09 08:29:49 +0000

    Commit auto_update

--- Disney/src/App.jsx
+++ Disney/src/App.jsx
@@ -6,9 +6,7 @@ import './App.css'
 import React from 'react'
 
 const App = () => {
-  return (
-    <div></div>
-  )
+  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App
