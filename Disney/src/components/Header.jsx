24add0cce3ab8c4029a0f27eca8af48a35d946b7
Joshua Owusu <joshbillion1@gmail.com>
2023-06-09 17:17:08 +0000

    Commit auto_update

--- Disney/src/components/Header.jsx
+++ Disney/src/components/Header.jsx
@@ -6,7 +6,7 @@ import { HiHome,
         HiPlayCircle,
         HiTv } from 'react-icons/hi2';
 import { HiPlus, HiDotsVertical } from 'react-icons/hi';
-import HeaderItem from './components/HeaderItem'
+import HeaderItem from './components/HeaderItem';
 
 function Header() {
 
