7786a6cbebba1e3574aa7d1fdb3715f8c679d54b
Joshua Owusu <joshbillion1@gmail.com>
2023-06-09 18:02:37 +0000

    Commit auto_update

--- Disney/src/components/Header.jsx
+++ Disney/src/components/Header.jsx
@@ -48,7 +48,7 @@ function Header() {
           ))}
         </div>
         <img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
-        <img src='' />
+        className='w-[30px]"/>
       </div>
     );
 }
