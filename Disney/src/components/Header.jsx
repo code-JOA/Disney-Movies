c276231fb5af87c0197f1193ee24f40b57b470b7
Joshua Owusu <joshbillion1@gmail.com>
2023-06-09 13:50:23 +0000

    Commit auto_update

--- Disney/src/components/Header.jsx
+++ Disney/src/components/Header.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import logo from './../assets/images/logo.png'
+import React from 'react';
+import logo from './../assets/images/logo.png';
 import { HiHome, 
     HiMagnifyingGlass,
     HiStar,
