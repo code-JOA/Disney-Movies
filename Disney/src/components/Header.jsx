211712e768e4140c89b829c7c3ff938950c692ff
Joshua Owusu <joshbillion1@gmail.com>
2023-06-09 18:16:32 +0000

    Commit auto_update

--- Disney/src/components/Header.jsx
+++ Disney/src/components/Header.jsx
@@ -43,7 +43,7 @@ function Header() {
             className="w-[80px] 
         md:w-[115px] object-cover"
           />
-          <div className='flex gap-8'>
+          <div className='hidden md:flex gap-8'>
             {menu.map((item) => (
               <HeaderItem name={item.name} Icon={item.icon} />
             ))}
