e1584baf45694b40eaaac1c555be0598be9a70de
Joshua Owusu <joshbillion1@gmail.com>
2023-06-09 18:03:44 +0000

    Commit auto_update

--- Disney/src/components/Header.jsx
+++ Disney/src/components/Header.jsx
@@ -37,7 +37,7 @@ function Header() {
     ]
     return (
       <div className="flex items-center gap-8">
-        <div>
+        <div class>
           <img
             src={logo}
             className="w-[80px] 
@@ -48,7 +48,7 @@ function Header() {
           ))}
         </div>
         <img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
-        className='w-[30px]"/>
+        className='w-[30px] founded-full"/>
       </div>
     );
 }
