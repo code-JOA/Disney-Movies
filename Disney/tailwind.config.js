ff1ed4c5a83958dc9541afe62607fe536651c33c
Joshua Owusu <joshbillion1@gmail.com>
2023-06-09 17:51:00 +0000

    Commit auto_update

--- Disney/tailwind.config.js
+++ Disney/tailwind.config.js
@@ -1,13 +1,3 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: ["./src/**/*.{html,js}"],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
-
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
