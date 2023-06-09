480cef8e37ee7cdc5809cd3f6937afc9da3f7772
Joshua Owusu <joshbillion1@gmail.com>
2023-06-09 17:37:21 +0000

    Commit auto_update

--- Disney/src/components/HeaderItem.jsx
+++ Disney/src/components/HeaderItem.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function HeaderItem({name, Icon}) {
     return (
     <div className='text-white flex items-center gap-3
-    text-[18px] fony-semibold cursor-pointer'>
+    text-[18px] font-semibold cursor-pointer'>
         <Icon/>
         <h2>{name}</h2>    
     </div>
