341593e566f8f0fa378e24f2293183b17db7c9f3
Joshua Owusu <joshbillion1@gmail.com>
2023-06-09 19:36:48 +0000

    Commit auto_update

--- Disney/src/components/Header.jsx
+++ Disney/src/components/Header.jsx
@@ -55,7 +55,7 @@ function Header() {
             <div>
                 {menu.map((item,index)=>index<2&&(
                 <HeaderItem name={''} Icon={item.icon} />
-                <div></div>
+                </div>
             </div>
           </div>
         </div>
