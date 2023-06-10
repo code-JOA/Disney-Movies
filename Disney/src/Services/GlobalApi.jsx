8d6e84a41dc086d5dbfbf4e4faa88022bb3d00f2
Joshua Owusu <joshbillion1@gmail.com>
2023-06-10 07:41:28 +0000

    Commit auto_update

--- Disney/src/Services/GlobalApi.jsx
+++ Disney/src/Services/GlobalApi.jsx
@@ -1,7 +1,7 @@
-// import axis from 'axios';
+import axios from 'axios';
 
-// const movieBaseUrl ='https://api.themoviedb.org/3'
-// const api_key = "de0b39d4bd85599f2e0dbce743bb0857"
+const movieBaseUrl ='https://api.themoviedb.org/3'
+const api_key = "de0b39d4bd85599f2e0dbce743bb0857"
 
 // // https://api.themoviedb.org/3/trending/all/day?api_key=de0b39d4bd85599f2e0dbce743bb0857
 const movieByGenreBaseURL='https://api.themoviedb.org/3/discover/movie?api_key=de0b39d4bd85599f2e0dbce743bb0857';
@@ -17,20 +17,20 @@ export default{
     getMovieByGenreId
 }
 
-import axios from "axios";
+// import axios from "axios";
 
-const movieBaseUrl="https://api.themoviedb.org/3"
-const api_key='de0b39d4bd85599f2e0dbce743bb0857'
+// const movieBaseUrl="https://api.themoviedb.org/3"
+// const api_key='de0b39d4bd85599f2e0dbce743bb0857'
 
-const movieByGenreBaseURL='https://api.themoviedb.org/3/discover/movie?api_key=de0b39d4bd85599f2e0dbce743bb0857';
+// const movieByGenreBaseURL='https://api.themoviedb.org/3/discover/movie?api_key=de0b39d4bd85599f2e0dbce743bb0857';
 
-//https://api.themoviedb.org/3/trending/all/day?api_key=de0b39d4bd85599f2e0dbce743bb0857
-const getTrendingVideos=axios.get(movieBaseUrl+
-    "/trending/all/day?api_key="+api_key);
-    const getMovieByGenreId=(id)=>
-    axios.get(movieByGenreBaseURL+"&with_genres="+id)
+// //https://api.themoviedb.org/3/trending/all/day?api_key=de0b39d4bd85599f2e0dbce743bb0857
+// const getTrendingVideos=axios.get(movieBaseUrl+
+//     "/trending/all/day?api_key="+api_key);
+//     const getMovieByGenreId=(id)=>
+//     axios.get(movieByGenreBaseURL+"&with_genres="+id)
 
-export default{
-    getTrendingVideos,
-    getMovieByGenreId
-}
+// export default{
+//     getTrendingVideos,
+//     getMovieByGenreId
+// }
