83a1061176bcf744adc92cc17238c93bef45ce52
Joshua Owusu <joshbillion1@gmail.com>
2023-06-10 07:45:41 +0000

    Commit auto_update

--- Disney/src/Services/GlobalApi.jsx
+++ Disney/src/Services/GlobalApi.jsx
@@ -8,7 +8,7 @@ const movieByGenreBaseURL =
   "https://api.themoviedb.org/3/discover/movie?api_key=de0b39d4bd85599f2e0dbce743bb0857";
 
 const getTrendingVideos=axios.get(movieBaseUrl+
-    "/trending/all/day?"+api_key);
+    "/trending/all/day?api_key="+api_key);
     const getMovieByGenreId=(id)=>
     axios.get(movieByGenreBaseURL+"&with_genres="+id)
 
@@ -18,20 +18,3 @@ export default{
     getMovieByGenreId
 }
 
-// import axios from "axios";
-
-// const movieBaseUrl="https://api.themoviedb.org/3"
-// const api_key='de0b39d4bd85599f2e0dbce743bb0857'
-
-// const movieByGenreBaseURL='https://api.themoviedb.org/3/discover/movie?api_key=de0b39d4bd85599f2e0dbce743bb0857';
-
-// //https://api.themoviedb.org/3/trending/all/day?api_key=de0b39d4bd85599f2e0dbce743bb0857
-// const getTrendingVideos=axios.get(movieBaseUrl+
-//     "/trending/all/day?api_key="+api_key);
-//     const getMovieByGenreId=(id)=>
-//     axios.get(movieByGenreBaseURL+"&with_genres="+id)
-
-// export default{
-//     getTrendingVideos,
-//     getMovieByGenreId
-// }
