efd0b8865808880edee2e3b8bfb14154fead51bf
Joshua Owusu <joshbillion1@gmail.com>
2023-06-10 07:38:13 +0000

    Commit auto_update

--- Disney/src/Services/GlobalApi.jsx
+++ Disney/src/Services/GlobalApi.jsx
@@ -4,12 +4,12 @@
 // const api_key = "de0b39d4bd85599f2e0dbce743bb0857"
 
 // // https://api.themoviedb.org/3/trending/all/day?api_key=de0b39d4bd85599f2e0dbce743bb0857
-
+const
 
 // const getTrendingVideos=axios.get(movieBaseUrl +
 //     "/trending/all/day?"+api_key);
     const getMovieByGenreId=(id)=>
-    axios
+    axios.get(movieByGenreBaseURL+"&with_genres="+id)
 
 
 // export default{
