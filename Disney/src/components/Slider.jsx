98cefd24432d68d15f91bee64cc704ee3af04809
Joshua Owusu <joshbillion1@gmail.com>
2023-06-10 09:49:21 +0000

    Commit auto_update

--- Disney/src/components/Slider.jsx
+++ Disney/src/components/Slider.jsx
@@ -18,10 +18,10 @@ function Slider() {
   };
 
   const sliderRight=(element)=>{
-    element.scrollLeft+=screenWidth-110
+    element.scrollLeft+=screenWidth-110;
   };
   const sliderLeft=(element) =>{
-    element.scrollLeft-=screenWidth-110
+    element.scrollLeft-=screenWidth-110;
   };
   return (
     <div>
