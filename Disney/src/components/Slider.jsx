6dfae75c44fc815be755fb51b16ab32669655dc2
Joshua Owusu <joshbillion1@gmail.com>
2023-06-10 09:40:46 +0000

    Commit auto_update

--- Disney/src/components/Slider.jsx
+++ Disney/src/components/Slider.jsx
@@ -1,32 +1,3 @@
-// import React, { useEffect } from "react";
-// import GlobalApi from '../Services/GlobalApi';
-
-// const IMAGE_BASE_URL='https://image.tmdb.org/t/p/orignal';
-
-// function Slider() {
-//     const [movieList,setMovieList]=useState([]);
-//     useEffect(()=>{
-//         getTrendingMovies();
-//     },[])
-
-//     const getTrendingMovies=()=>{
-//         GlobalApi.getTrendingVideos.then(resp=>{
-//             console.log(resp.data.results);
-//             setMovieList()
-//         })
-//     }
-//   return (
-//     <div>
-//         {movieList.map((item,index)=>(
-//             <img src={IMAGE_BASE_URL+item.backdrop_path} 
-//             className='min-w-full h-[310px]' />
-//         ))}
-//     </div>
-//   )
-// }
-
-// export default Slider
-
 import React, { useEffect, useRef, useState } from "react";
 import GlobalApi from "../Services/GlobalApi";
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
