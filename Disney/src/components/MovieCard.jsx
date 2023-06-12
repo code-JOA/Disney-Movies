dd2a059dc400c54b23db325c68ebd9a7dbd49217
Joshua Owusu <joshbillion1@gmail.com>
2023-06-12 14:31:32 +0000

    Commit auto_update

--- Disney/src/components/MovieCard.jsx
+++ Disney/src/components/MovieCard.jsx
@@ -10,7 +10,7 @@ function MovieCard({movie}) {
         className='w-[110px] md:w-[200px] rounded-lg
         hover:border-[3px] border-gray-400
         hover:scale-110 transition-all duration-150
-        ease-in-out '/>
+        ease-in'/>
     </>
   )
 }
