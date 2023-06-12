b8e3085f8950a66be28846bab3ea491c10debfc3
Joshua Owusu <joshbillion1@gmail.com>
2023-06-12 10:32:22 +0000

    Commit auto_update

--- Disney/src/components/GenreMovieList.jsx
+++ Disney/src/components/GenreMovieList.jsx
@@ -1,18 +1,26 @@
-// import React from 'react'
-// import GenresList from './Constant/GenresList'
+import React from "react";
+import GenresList from "../Constant/GenresList";
+import MovieList from "./MovieList";
 
-// function GenreMovieList() {
-//     return (
-//     <div>
-//         {GenresList.genere.map((item,index)=>index<=4&&(
-//         <div className='p-8 px-8 md:px-16'>
-//             <h2 className='text-[20px] text-white 
-//             font-bold'>{item.name}</h2>
-//             <MovieList/>
-//         </div>
-//         ))}
-//         </div>
-//     )
-// }
+function GenreMovieList() {
+  return (
+    <div>
+      {GenresList.genere.map(
+        (item, index) =>
+          index <= 4 && (
+            <div className="p-8 px-8 md:px-16">
+              <h2
+                className="text-[20px] text-white 
+                font-bold"
+              >
+                {item.name}
+              </h2>
+              <MovieList genreId={item.id} index_={index} />
+            </div>
+          )
+      )}
+    </div>
+  );
+}
 
-// export default GenreMovieList
+export default GenreMovieList;
