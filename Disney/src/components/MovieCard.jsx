ca9a669aef297c2a88339e0d753a4a938d225045
Joshua Owusu <joshbillion1@gmail.com>
2023-06-12 13:57:37 +0000

    Commit auto_update

--- Disney/src/components/MovieCard.jsx
+++ Disney/src/components/MovieCard.jsx
@@ -6,7 +6,7 @@ const IMAGE_BASE_URL='https://image.tmdb.org/t/p/original';
 function MovieCard({movie}) {
   return (
     <div>
-        <img src={IMAGE_BASE_URL+movie.poster_path} ></img>
+        <img src={IMAGE_BASE_URL+movie.poster_path} />
     </div>
   )
 }
