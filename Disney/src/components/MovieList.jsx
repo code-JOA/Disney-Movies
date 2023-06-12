56906adea6036e0431a9373cf4f162a7145b4af1
Joshua Owusu <joshbillion1@gmail.com>
2023-06-12 11:05:00 +0000

    Commit auto_update

--- Disney/src/components/MovieList.jsx
+++ Disney/src/components/MovieList.jsx
@@ -2,6 +2,9 @@ import React, { useEffect }  from 'react'
 import GlobalApi from '../Services/GlobalApi'
 
 function MovieList({genreId}) {
+    useEffect(()=>{
+
+    },[])
 
     const getMovieByGenreId=()=>{
         GlobalApi.getMovieByGenreId(genreId).then(resp)=>{
