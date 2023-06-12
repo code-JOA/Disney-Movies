2bfd3ba41417f56ee56aa60a603b35fb4be30468
Joshua Owusu <joshbillion1@gmail.com>
2023-06-12 15:01:03 +0000

    Commit auto_update

--- Disney/src/components/MovieList.jsx
+++ Disney/src/components/MovieList.jsx
@@ -37,9 +37,9 @@ function MovieList({genreId}) {
         scrollbar-none scroll-smooth pt-4 px-3 pb-4">
           {movieList.map((item,index)=>(
             <>
-            {index_%3==0?<HrMovieCard movie={item}
+            {index_%3==0?<HrMovieCard movie={item}/>:<MovieCard movie={item} />
+
             </>
-            // <MovieCard movie={item} />
           ))}
         </div>
 
