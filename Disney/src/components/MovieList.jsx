b20bc7bec359495bdc1dcd243602ed1d3959a45b
Joshua Owusu <joshbillion1@gmail.com>
2023-06-12 14:51:06 +0000

    Commit auto_update

--- Disney/src/components/MovieList.jsx
+++ Disney/src/components/MovieList.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState }  from 'react'
 import GlobalApi from '../Services/GlobalApi'
 import MovieCard from './MovieCard'
 
+
 function MovieList({genreId}) {
     const [movieList, setMovieList]=useState([])
     useEffect(()=>{
@@ -16,27 +17,33 @@ function MovieList({genreId}) {
     }
 
     return (
-        <div>
-            <IoChevronBackOutline onClick={()=>
-            slideLeft(elementRef.current)} 
-            className='text-[50px]
-            text-white p-2 z-10 cursor-pointer
-            hidden md:block absolute 
-            mt-[150px]}/>
+      <div className="relative">
+        <IoChevronBackOutline
+          onClick={() => slideLeft(elementRef.current)}
+          className={`text-[50px] text-white p-2 z-10
+        cursor-pointer hidden md:block absolute ${
+          index_ % 3 == 0 ? "mt-[80px]" : "mt-[150px]"
+        } `}
+        />
 
-   <div className='relative'>
-        <IoChevronBackOutline onClick={()=>
-        slideLeft(elementRef.current)} 
-        className={`text-[50px] text-white p-2 z-10
-        cursor-pointer hidden md:block absolute ${index_%3==0?'mt-[80px]':'mt-[150px]'} `}/>
-
-        <div className='flex overflow-x-auto gap-8
-        scrollbar-none pt-5 px-3 pb-5'>
-            {movieList.map((item,index)=>(
-                <MovieCard movie={item}/>
-            ))}
+        <div
+          className="flex overflow-x-auto gap-8
+        scrollbar-none pt-5 px-3 pb-5"
+        >
+          {movieList.map((item, index) => (
+            <MovieCard movie={item} />
+          ))}
         </div>
-    )
+
+        <IoChevronForwardOutline
+          onClick={() => slideRight(elementRef.current)}
+          className={`text-[50px] text-white hidden md:block
+           p-2 cursor-pointer z-10 top-0
+            absolute right-0 
+            ${index_ % 3 == 0 ? "mt-[80px]" : "mt-[150px]"}`}
+        />
+      </div>
+    );
 }
 
 export default MovieList
