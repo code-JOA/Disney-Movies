d266fb46dd27ba8ccd30df2dc8ce3760e62dce91
Joshua Owusu <joshbillion1@gmail.com>
2023-06-12 10:11:59 +0000

    Commit auto_update

--- Disney/src/components/GenreMovieList.jsx
+++ Disney/src/components/GenreMovieList.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import GenreList from './components/GenreList.jsx'
 
-const GenreMovieList = () => {
-  return (
+const GenreMovieList() {
+    return (
     <div>
-        {GenresList.genere.map((item,item)=>
+        {GenresList.genere.map((item,index)=>
         <div>
             <h2>{item.name}</h2>
         </div>
