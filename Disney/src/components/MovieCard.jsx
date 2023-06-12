8782b67f573c22baded753c8675eaa8753339b27
Joshua Owusu <joshbillion1@gmail.com>
2023-06-12 14:26:03 +0000

    Commit auto_update

--- Disney/src/components/MovieCard.jsx
+++ Disney/src/components/MovieCard.jsx
@@ -7,7 +7,9 @@ function MovieCard({movie}) {
   return (
     <>
         <img src={IMAGE_BASE_URL+movie.poster_path}
-        className='w-[110px] md:w-[200px] rounded-lg'/>
+        className='w-[110px] md:w-[200px] rounded-lg
+        hover:border-[3px] border-gray-400
+        hover:scale-110'/>
     </>
   )
 }
