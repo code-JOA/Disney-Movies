6ec42fde2ccb5a06f6f588d59fe538046b47bdeb
Joshua Owusu <joshbillion1@gmail.com>
2023-06-12 16:48:05 +0000

    Commit auto_update

--- Disney/src/components/MovieList.jsx
+++ Disney/src/components/MovieList.jsx
@@ -54,9 +54,10 @@ function MovieList({ genreId, index_ }) {
       </div>
       <IoChevronForwardOutline
         onClick={() => slideRight(elementRef.current)}
-        className={`text-[50px] text-white hidden md:block
+        className=
+        {`text-[50px] text-white hidden md:block
         p-2 cursor-pointer z-10 top-0 absolute right-0
-        ${index_%3==0?'mt-[80px]:"mt-[150px]'} `}/>
+        ${index_%3==0?'mt-[80px]':'mt-[150px]'} `}/>
 
 
         {`text-[50px] text-white hidden md:block
