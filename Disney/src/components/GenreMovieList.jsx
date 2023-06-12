9b2675de7731b59878937356bf4204fb1eacd6d6
Joshua Owusu <joshbillion1@gmail.com>
2023-06-12 10:26:59 +0000

    Commit auto_update

--- Disney/src/components/GenreMovieList.jsx
+++ Disney/src/components/GenreMovieList.jsx
@@ -1,43 +1,16 @@
-// import React from 'react'
-// import GenresList from './Constant/GenresList'
-
-// function GenreMovieList() {
-//     return (
-//     <div>
-//         {GenresList.genere.map((item,index)=>index<=4&&(
-//         <div>
-//             <h2 className='text-[20px] text-white'>{item.name}</h2>
-//         </div>
-//         ))}
-//         </div>
-//     )
-// }
-
-// export default GenreMovieList
-
-import React from "react";
-import GenresList from "../Constant/GenresList";
-// import MovieList from "./MovieList";
+import React from 'react'
+import GenresList from './Constant/GenresList'
 
 function GenreMovieList() {
-  return ( 
+    return (
     <div>
-      {GenresList.genere.map(
-        (item, index) =>
-          index <= 4 && (
-            <div className="p-8 px-8 md:px-16">
-              <h2
-                className="text-[20px] text-white 
-                font-bold"
-              >
-                {item.name}
-              </h2>
-              <MovieList genreId={item.id} index_={index} />
-            </div>
-          )
-      )}
-    </div>
-  );
+        {GenresList.genere.map((item,index)=>index<=4&&(
+        <div>
+            <h2 className='text-[20px] text-white font-bold'>{item.name}</h2>
+        </div>
+        ))}
+        </div>
+    )
 }
 
-export default GenreMovieList;
+export default GenreMovieList
