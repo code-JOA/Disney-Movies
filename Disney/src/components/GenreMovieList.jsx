204bc602c07f0f7dd20b12d0c77cfec573538685
Joshua Owusu <joshbillion1@gmail.com>
2023-06-12 10:28:03 +0000

    Commit auto_update

--- Disney/src/components/GenreMovieList.jsx
+++ Disney/src/components/GenreMovieList.jsx
@@ -5,8 +5,9 @@ function GenreMovieList() {
     return (
     <div>
         {GenresList.genere.map((item,index)=>index<=4&&(
-        <div>
-            <h2 className='text-[20px] text-white font-bold'>{item.name}</h2>
+        <div className='p-8 px-8 md:px-16'>
+            <h2 className='text-[20px] text-white 
+            font-bold'>{item.name}</h2>
         </div>
         ))}
         </div>
