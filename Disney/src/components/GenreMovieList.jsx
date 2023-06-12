fef3e081ae9628d85bb1ad0a6aaa4f0d51bf6a96
Joshua Owusu <joshbillion1@gmail.com>
2023-06-12 13:14:41 +0000

    Commit auto_update

--- Disney/src/components/GenreMovieList.jsx
+++ Disney/src/components/GenreMovieList.jsx
@@ -5,12 +5,9 @@ import MovieList from "./MovieList";
 function GenreMovieList() {
   return (
     <div>
-      {GenresList.genere.map(
-        (item, index) =>
-          index <= 4 && (
+      {GenresList.genere.map((item, index) => index <= 4 && (
             <div className="p-8 px-8 md:px-16">
-              <h2
-                className="text-[20px] text-white 
+              <h2 className="text-[20px] text-white 
                 font-bold"
               >
                 {item.name}
