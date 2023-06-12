f00af0a52b0ee5e55bf85f68f33e6d0a98d5b314
Joshua Owusu <joshbillion1@gmail.com>
2023-06-12 16:23:23 +0000

    Commit auto_update

--- Disney/src/components/HrMovieCard.jsx
+++ Disney/src/components/HrMovieCard.jsx
@@ -1,34 +1,26 @@
-// import React from 'react'
 
-// function HrMovieCard() {
-//   return (
-//     <div>HrMovieCard</div>
-//   )
-// }
-
-// export default HrMovieCard
 
 
 // import React from "react";
-// const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
 
-// function HrMovieCard({ movie }) {
-//   return (
-//     <section className="hover:scale-110 transition-all duration-150 ease-in">
-//       <img
-//         src={IMAGE_BASE_URL + movie.backdrop_path}
-//         className="w-[110px] md:w-[260px] rounded-lg
-//     hover:border-[3px] border-gray-400 cursor-pointer
-//     "
-//       />
-//       <h2
-//         className="w-[110px] md:w-[260px] text-white
-//     mt-2"
-//       >
-//         {movie.title}
-//       </h2>
-//     </section>
-//   );
-// }
+function HrMovieCard({ movie }) {
+  return (
+    <section className="hover:scale-110 transition-all duration-150 ease-in">
+      <img
+        src={IMAGE_BASE_URL + movie.backdrop_path}
+        className="w-[110px] md:w-[260px] rounded-lg
+    hover:border-[3px] border-gray-400 cursor-pointer
+    "
+      />
+      <h2
+        className="w-[110px] md:w-[260px] text-white
+    mt-2"
+      >
+        {movie.title}
+      </h2>
+    </section>
+  );
+}
 
-// export default HrMovieCard;
+export default HrMovieCard;
