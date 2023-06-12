e822df3407c3baf63f24adedb763cc7fe1f2c6b9
Joshua Owusu <joshbillion1@gmail.com>
2023-06-12 09:16:25 +0000

    Commit auto_update

--- Disney/src/components/ProductionHouse.jsx
+++ Disney/src/components/ProductionHouse.jsx
@@ -48,13 +48,14 @@ function ProductionHouse() {
             rounded-lg hover:scale-110 transition-all duration-300 
             erase-in-out cursor-pointer"
         >
-          <img src={item.image} className="w-full" />
+          <img src={item.image} className="w-full z-[1]" />
           <video
             src={item.video}
             autoPlay
             loop
             playsInline
-            className="absolute top-0 round-md z-0"
+            className="absolute top-0 round-md z-0 opacity-0 
+            hover:opacity-50"
           >
           </video>
         </div>
