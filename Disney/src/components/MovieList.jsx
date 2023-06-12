4c355ec985810ce30e3fe31b23c5796a764a7ad8
Joshua Owusu <joshbillion1@gmail.com>
2023-06-12 15:25:44 +0000

    Commit auto_update

--- Disney/src/components/MovieList.jsx
+++ Disney/src/components/MovieList.jsx
@@ -36,9 +36,9 @@ function MovieList({ genreId, index_ }) {
 
     <div ref={elementRef}
         className="flex overflow-x-auto gap-8
-        scrollbar-none scroll-smooth pt-4 px-3 pb-4">
+        scrollbar-none pt-5 px-3 pb-5">
             {movieList.map((item,index)=>(
-
+                <MovieCard
         ))}
     </div>
 
